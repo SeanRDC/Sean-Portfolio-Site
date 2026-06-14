8cc0a6efd146e0d633194a173204c0cfd34f6d9a
SeanRDC <delacruzseanrhani@gmail.com>
2026-06-15 06:22:29 +0800

    removed

--- PortfolioV2/src/App.tsx
+++ PortfolioV2/src/App.tsx
@@ -1,11 +1,6 @@
-import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes";
 import "./index.css";
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
-  );
+  return <AppRoutes />;
 }
