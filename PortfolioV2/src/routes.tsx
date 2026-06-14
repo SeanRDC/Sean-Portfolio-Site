51703ae4ba59cdfcf1ae08c9aca2023cd0955340
SeanRDC <delacruzseanrhani@gmail.com>
2026-06-15 06:17:19 +0800

    dynamic title

--- PortfolioV2/src/routes.tsx
+++ PortfolioV2/src/routes.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import DocumentTitle from "./components/DocumentTitle"; // Import the new helper
 import Layout from "./components/Layout";
 import HomeView from "./screens/HomeView";
 import WorkView from "./screens/WorkView";
@@ -7,13 +8,16 @@ import CertificatesView from "./screens/CertificatesView";
 
 export default function AppRoutes() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<HomeView />} />
-        <Route path="/work" element={<WorkView />} />
-        <Route path="/project/:id" element={<ProjectView />} />
-        <Route path="/certificates" element={<CertificatesView />} />
-      </Route>
-    </Routes>
+    <>
+      <DocumentTitle />
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<HomeView />} />
+          <Route path="/work" element={<WorkView />} />
+          <Route path="/project/:id" element={<ProjectView />} />
+          <Route path="/certificates" element={<CertificatesView />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
