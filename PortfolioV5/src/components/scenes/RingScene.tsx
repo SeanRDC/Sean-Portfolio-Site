e2a2676a6c1cdc0f11080668bd1713749bd5f93d
SeanRDC <delacruzseanrhani@gmail.com>
2026-07-03 04:22:42 +0800

    removed unused declaratioins

--- PortfolioV5/src/components/scenes/RingScene.tsx
+++ PortfolioV5/src/components/scenes/RingScene.tsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from "react";
+import { useRef} from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
