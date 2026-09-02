27b4875bdc373cb155b07e7d9290ae37fc631a9b
SeanRDC <delacruzseanrhani@gmail.com>
2026-09-02 16:48:52 +0800

    Removed preloader

--- PortfolioV5/src/App.tsx
+++ PortfolioV5/src/App.tsx
@@ -1,8 +1,6 @@
-import { useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes";
 import SmoothScroll from "./components/SmoothScroll";
-import Preloader from "./components/Preloader";
 import AnalyticsTracker from "./AnalyticsTracker";
 import { reportWebVitals } from "./reportWebVitals";
 import "./index.css";
@@ -11,23 +9,12 @@ import "./index.css";
 reportWebVitals();
 
 export default function App() {
-  const [isLoaded, setIsLoaded] = useState(false);
-
   return (
     <BrowserRouter>
       <AnalyticsTracker />
-
-      {!isLoaded && <Preloader onComplete={() => setIsLoaded(true)} />}
-      <div
-        style={{
-          opacity: isLoaded ? 1 : 0,
-          transition: "opacity 0.5s ease-in-out",
-        }}
-      >
-        <SmoothScroll>
-          <AppRoutes />
-        </SmoothScroll>
-      </div>
+      <SmoothScroll>
+        <AppRoutes />
+      </SmoothScroll>
     </BrowserRouter>
   );
 }
